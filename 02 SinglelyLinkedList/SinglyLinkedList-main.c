7a2ec85bed7fd563640714b4f6e52d2650fdb908
18056074197 <18056074197@163.com>
2020-09-14 17:03:54 +0800

    单链表-完全运行版

--- 02 SinglelyLinkedList/SinglyLinkedList-main.c
+++ 02 SinglelyLinkedList/SinglyLinkedList-main.c
@@ -10,62 +10,62 @@ int main(int argc,char **argv){
     LinkList L;
     int i;
     LElemType_L e;
-    printf("01.函数InitLIst_L 测试...\n");
-    {
-        printf("初始化单链表L ...\n");
-        InitList_L(&L);
-        printf("\n");
-    }
-    PressEnter
-    printf("04.函数ListEmpty_L 测试...\n");
-    {
-        ListEmpty_L(L)?printf("L 为空！！\n"):printf("L 不为空！\n");
-        printf("\n");
-    }
-    PressEnter
-    printf("10.函数 ListInsert_L 测试...\n");
-    {
-        for ( i = 1; i <=6 ; ++i) {
-            printf("在L第%d个位置插入\"%d\"...\n ", i, 2 * i);
-            ListInsert_L(L, i, 2 * i);
-
-        }
-        printf("\n");
-    }
-    PressEnter
-    printf("12函数 ListTraverse_L 测试...\n");
-    {
-        printf("L中的元素为：L=");
-        ListTraverse_L(L, PrintElem);
-        printf("\n\n");
-    }
-    PressEnter
-    printf("05.函数ListLength_L测试..\n");
-    {
-        printf("L的长度为%d \n", ListLength_L(L));
-        printf("\n");
-    }
-    PressEnter
-    printf("11.函数GetElem_L 测试...\n");
-    {
-        GetElem_L(L, 4, &e);
-        printf("L中第4个位置的元素为\"%d\" \n", e);
-        printf("\n");
-    }
-    PressEnter
-    printf("08函数PriorElem_L测试...\n");
-    {
-        PriorElem_L(L, 6, &e);
-        printf("元素\" 6\" 的后继为\"%d\" \n", e);
-        printf("\n");
-    }
+//    printf("01.函数InitLIst_L 测试...\n");
+//    {
+//        printf("初始化单链表L ...\n");
+//        InitList_L(&L);
+//        printf("\n");
+//    }
+//    PressEnter
+//    printf("04.函数ListEmpty_L 测试...\n");
+//    {
+//        ListEmpty_L(L)?printf("L 为空！！\n"):printf("L 不为空！\n");
+//        printf("\n");
+//    }
+//    PressEnter
+//    printf("10.函数 ListInsert_L 测试...\n");
+//    {
+//        for ( i = 1; i <=6 ; ++i) {
+//            printf("在L第%d个位置插入\"%d\"...\n ", i, 2 * i);
+//            ListInsert_L(L, i, 2 * i);
+//
+//        }
+//        printf("\n");
+//    }
+//    PressEnter
+//    printf("12函数 ListTraverse_L 测试...\n");
+//    {
+//        printf("L中的元素为：L=");
+//        ListTraverse_L(L, PrintElem);
+//        printf("\n\n");
+//    }
+//    PressEnter
+//    printf("05.函数ListLength_L测试..\n");
+//    {
+//        printf("L的长度为%d \n", ListLength_L(L));
+//        printf("\n");
+//    }
+//    PressEnter
+//    printf("11.函数GetElem_L 测试...\n");
+//    {
+//        GetElem_L(L, 4, &e);
+//        printf("L中第4个位置的元素为\"%d\" \n", e);
+//        printf("\n");
+//    }
+//    PressEnter
+//    printf("08函数PriorElem_L测试...\n");
+//    {
+//        PriorElem_L(L, 6, &e);
+//        printf("元素\" 6\" 的后继为\"%d\" \n", e);
+//        printf("\n");
+//    }
     PressEnter
     printf("13.函数 CreateList_HL测试...\n");
     {
         FILE *fp;
         LinkList L;
         printf("头插法建立单链表L=");
-        fp = fopen("TestData_HL.txt", "r");
+        fp = fopen("TestData_HL.txt", "r");//备注CLion读取文件时必须将文件放入cmake-build-debug目录下才能读取
         CreatList_HL(fp, &L, 5);
         fclose(fp);
         ListTraverse_L(L, PrintElem);
